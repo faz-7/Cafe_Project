4442f6ad522e1cde50be7d66b4bf18aaaf190efb
faz-7 <112781265+faz-7@users.noreply.github.com>
2022-12-01 07:05:19 +0330

    add user directory to put utilities

--- user/utils.py
+++ user/utils.py
@@ -1,25 +1,13 @@
-from Cafe_Project.models.user import User
 from Cafe_Project.core.db_manager import session
+from Cafe_Project.models.user import User
+from Cafe_Project.models.receipts import Receipt
+from Cafe_Project.table.utils import assign_table
 
 
-def add_user(username, fname, lname, phone, email, password):
-    user = User(username=username, fname=fname, lname=lname, phone=phone, email=email, password=password)
-    session.add(user)
-    session.commit()
-
-
-def check_login(username, password):
-    # question = session.query(User).filter_by(id=1).all()
-    users = session.query(User).all()
-    for q in users:
-        if str(q.username) == username:
-            if str(q.password) == password:
-                return True
-
-
-def check_username(username):
-    users = session.query(User).all()
-    for q in users:
-        if str(q.username) == username:
-            return False
-    return True
+def create_receipt(password):
+    user_id = session.query(User).filter_by(password=password).first().id
+    if assign_table():
+        Receipt.add(id=user_id, user_id=user_id, table_id=assign_table(), total_price=0, pay=False)
+    else:
+        pass
+        # todo: check this for first add to cart
