df105874d64a17d33452d8dc2bae57c743331fb9
faz-7 <112781265+faz-7@users.noreply.github.com>
2022-12-02 15:01:01 +0330

    update app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask, render_template, url_for, request, redirect
-
-from models.user import User
-from models.table import Table
-from models.receipts import Receipt
 from Cafe_Project.menu_items.utils import MenuItems, get_item
+from Cafe_Project.user.utils import create_receipt, check_login, add_user
 from Cafe_Project.table.utils import assign_table
-from Cafe_Project.receipt.utils import get_id
-from Cafe_Project.user.utils import create_receipt
+from Cafe_Project.receipt.utils import get_id, get_receipt, total_amount, add
 
 app = Flask(__name__)
 
@@ -34,7 +30,7 @@ def sign_up():
     email = request.form['email']
     phone = request.form['phone']
     password = request.form['password']
-    User.add(4, first_name, last_name, phone, email, password)
+    add_user(4, first_name, last_name, phone, email, password)
     return render_template('index.html')
 
 
@@ -52,25 +48,12 @@ def menu_item():
     return render_template('menu_item.html', items=items)
 
 
-@app.route('/receipt')
-def receipt():
-    pass
-
-
-# @app.route('/create_receipt/<integer:id>', methods=['GET', 'POST'])
-# def create_receipt(id):
-#     if assign_table():
-#         Receipt.add(id=id, user_id=id, table_id=assign_table(), total_price=0, pay=False)
-#     else:
-#         pass
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            if User.check_login(request.form['username'], request.form['password']):
+            if check_login(request.form['username'], request.form['password']):
                 create_receipt(request.form['password'])
                 return redirect(url_for('home'))
             else:
@@ -83,6 +66,21 @@ def login():
             error = 'Invalid Credentials. Please try again.'
 
     return render_template('login.html', error=error)
+
+
+@app.route('/create_receipt/<string:id>', methods=['GET', 'POST'])
+def create_receipt(id):
+    if assign_table():
+        add(id=id, user_id=id, table_id=assign_table(), total_price=0, pay=False)
+    else:
+        pass
+
+
+@app.route('/show_receipt/<string:id>', methods=['GET', 'POST'])
+def show_receipt(id):
+    receipts = get_receipt(id)
+    total = total_amount(id)
+    return render_template('receipt.html', receipts=receipts, total=total)
 
 
 if __name__ == '__main__':
